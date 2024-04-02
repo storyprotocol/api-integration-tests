e5ef088a77d3c2d2ed9d6f0bf0d6c05e93785da8
Tina-57blocks <tina@57blocks.com>
2024-04-02 13:05:36 +0800

    Update CI run for sepolia & renaissance

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,21 +7,24 @@ import { defineConfig } from '@playwright/test';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const EnvName = process.env.test_env || '';
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
+  globalSetup: require.resolve('./global-setup'),
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 6 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { outputFolder: `playwright-report-${EnvName}` }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
